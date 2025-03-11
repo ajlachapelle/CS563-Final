1809681cfaa1cebe7476bd79c66e2159ca94e6f6
ajlachapelle <alachape@uoregon.edu>
2025-03-11 14:38:36 -0700

    Updated synthesis script

--- synthetic-tests/synthesis.py
+++ synthetic-tests/synthesis.py
@@ -4,19 +4,26 @@ n = int(sys.argv[1])
 
 with open('template_header.c', 'r', encoding='utf-8') as input:
     header = input.read()
-    header += '\n\n'
 
 with open('template_body.c', 'r', encoding='utf-8') as input:
     body = input.read()
 
 # Generate fpgen programs
+new_header = header + '\n\n'
 block = 'injected_sum\n'
 for i in range(n):
     block += '\twhile (L[0])\n'
     new_body = body.replace('// FLAG\n', block + '\tinjected_sum;\n')
     with open('fpgen/synthesized' + str(i+1) + '.c', 'w', encoding='utf-8') as output:
-        output.write(header)
+        output.write(new_header)
         output.write(new_body)
 
 # Generate metrinome programs
-header += "_local" # modify the klee include statement
+new_header = header + '_local\n\n' # modify the klee include statement
+block = 'sum\n'
+for i in range(n):
+    block += '\twhile (L[0])\n'
+    new_body = body.replace('// FLAG\n', block + '\tsum;\n')
+    with open('metrinome/synthesized' + str(i+1) + '.c', 'w', encoding='utf-8') as output:
+        output.write(new_header)
+        output.write(new_body)
