54423bec6a6ce3e8f11fcc3d1c899b7e9abb3b05
ajlachapelle <alachape@uoregon.edu>
2025-03-11 15:49:46 -0700

    Updated synthesis for ease of use with metrinome

--- synthetic-tests/synthesis.py
+++ synthetic-tests/synthesis.py
@@ -5,6 +5,9 @@ n = int(sys.argv[1])
 with open('template_header.c', 'r', encoding='utf-8') as input:
     header = input.read()
 
+with open('template_defs.c', 'r', encoding='utf-8') as input:
+    defs = input.read()
+
 with open('template_body.c', 'r', encoding='utf-8') as input:
     body = input.read()
 
@@ -16,14 +19,19 @@ for i in range(n):
     new_body = body.replace('// FLAG\n', block + '\tinjected_sum;\n')
     with open('fpgen/synth' + str(i+1) + '.c', 'w', encoding='utf-8') as output:
         output.write(new_header)
-        output.write(new_body)
+        output.write(defs)
+        output.write('int main(int argc, char *argv[])\n' + new_body)
 
-# Generate metrinome programs
-new_header = header + '_local\n\n' # modify the klee include statement
-block = 'sum\n'
-for i in range(n):
-    block += '\twhile (L[0])\n'
-    new_body = body.replace('// FLAG\n', block + '\tsum;\n')
-    with open('metrinome/synth' + str(i+1) + '.c', 'w', encoding='utf-8') as output:
-        output.write(new_header)
+# Generate metrinome program
+# Each variation will be written to the same file for ease of use with Metrinome REPL
+with open('metrinome/synth.c', 'w', encoding='utf-8') as output:
+    new_header = header + '_local\n\n' # modify the klee include statement
+    output.write(new_header)
+    output.write(defs)
+    # 
+    block = 'sum\n'
+    for i in range(n):
+        block += '\twhile (L[0])\n'
+        new_body = 'int synth' + str(i+1) + '()'
+        new_body += body.replace('// FLAG\n', block + '\tsum;\n')
         output.write(new_body)
