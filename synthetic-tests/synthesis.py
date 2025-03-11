9b5aed3bf2f850d525a86cbc6177cec540eb80f1
ajlachapelle <alachape@uoregon.edu>
2025-03-11 15:03:46 -0700

    Updated synthesis script

--- synthetic-tests/synthesis.py
+++ synthetic-tests/synthesis.py
@@ -14,7 +14,7 @@ block = 'injected_sum\n'
 for i in range(n):
     block += '\twhile (L[0])\n'
     new_body = body.replace('// FLAG\n', block + '\tinjected_sum;\n')
-    with open('fpgen/synthesized' + str(i+1) + '.c', 'w', encoding='utf-8') as output:
+    with open('fpgen/synth' + str(i+1) + '.c', 'w', encoding='utf-8') as output:
         output.write(new_header)
         output.write(new_body)
 
@@ -24,6 +24,6 @@ block = 'sum\n'
 for i in range(n):
     block += '\twhile (L[0])\n'
     new_body = body.replace('// FLAG\n', block + '\tsum;\n')
-    with open('metrinome/synthesized' + str(i+1) + '.c', 'w', encoding='utf-8') as output:
+    with open('metrinome/synth' + str(i+1) + '.c', 'w', encoding='utf-8') as output:
         output.write(new_header)
         output.write(new_body)
